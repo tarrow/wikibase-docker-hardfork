bb53f8ea74790eb8b5a286cb59f14bfa7978e55b
Thomas Arrow <thomasarrow@gmail.com>
2018-05-04 10:55:58 +0100

    show branch in logs form setup script

--- travis/docker-push-setup.sh
+++ travis/docker-push-setup.sh
@@ -3,4 +3,9 @@
 #https://graysonkoonce.com/getting-the-current-branch-name-during-a-pull-request-in-travis-ci
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
+if [ "$BRANCH" == "master" ]
+then echo "This commit has been merged to master so on success images will be pushed"
+else echo "This is branch; $BRANCH so we won't be pushing"
+fi
+
 docker login -u $DOCKER_USER -p $DOCKER_PASS
