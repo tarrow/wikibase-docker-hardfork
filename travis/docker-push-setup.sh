30c0189b6205805f3ef3fe6c1a68c27093563581
Thomas Arrow <thomasarrow@gmail.com>
2018-05-04 10:38:18 +0100

    remove email from docker since fag is removed

--- travis/docker-push-setup.sh
+++ travis/docker-push-setup.sh
@@ -3,4 +3,4 @@
 #https://graysonkoonce.com/getting-the-current-branch-name-during-a-pull-request-in-travis-ci
 export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
