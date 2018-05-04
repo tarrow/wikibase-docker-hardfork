d4f44707466468b7b1e7f47c9f1fa5edff83ed9d
Thomas Arrow <thomasarrow@gmail.com>
2018-05-04 09:53:33 +0100

    TEST DNM to wmde TEST building only

--- wikibase/1.29/.travis/build-deploy.sh
+++ wikibase/1.29/.travis/build-deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker build "$DIR/../" -t wikibase/wikibase:1.29 -t wikibase/wikibase:legacy
+docker build "$DIR/../" -t tarrow/wikibase:1.29 -t tarrow/wikibase:legacy
 
 if [ "$BRANCH" == "master" ]
-    then docker push wikibase/wikibase:1.29 && docker push wikibase/wikibase:legacy
+then echo "pushing!" && docker push tarrow/wikibase:1.29 && docker push tarrow/wikibase:legacy
 fi
