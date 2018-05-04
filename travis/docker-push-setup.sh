5d4cc3a3942d5688e1b0621d33e7785da88d5d12
Thomas Arrow <thomasarrow@gmail.com>
2018-05-04 09:44:23 +0100

    Push from travis
    Setup docker pushing by loggin in using ENVs that will be stored
    encrypted in travis.
    
    Find the true branch and only push if on master

---
+++ travis/docker-push-setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#https://graysonkoonce.com/getting-the-current-branch-name-during-a-pull-request-in-travis-ci
+export BRANCH=$(if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then echo $TRAVIS_BRANCH; else echo $TRAVIS_PULL_REQUEST_BRANCH; fi)
+
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
