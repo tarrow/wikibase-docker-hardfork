aed20e0377d8dabb68ec06c07da6b8fa232a5fe5
Thomas Arrow <tarrow@users.noreply.github.com>
2018-05-04 13:13:52 +0100

    Revert "Push from travis test"

--- wdqs/0.2.5/.travis/build.sh
+++ wdqs/0.2.5/.travis/build.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker build "$DIR/../" -t wikibase/wdqs-proxy:latest
-
-if [ "$SHOULD_DOCKER_PUSH" = true ]; then
-    docker push tarrow/wdqs-proxy:latest
-fi
+docker build "$DIR/../" -t wikibase/wdqs:0.2.5
