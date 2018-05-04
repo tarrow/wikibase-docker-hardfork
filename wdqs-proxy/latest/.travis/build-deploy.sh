3f9a760000cabd4a50bb112236832ac05d87cff3
Thomas Arrow <thomasarrow@gmail.com>
2018-05-04 12:54:38 +0100

    DNM testing ONLY

--- wdqs-proxy/latest/.travis/build-deploy.sh
+++ wdqs-proxy/latest/.travis/build-deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker build "$DIR/../" -t wikibase/wdqs-proxy:latest
+docker build "$DIR/../" -t tarrow/wdqs-proxy:latest
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
-    docker push wikibase/wdqs-proxy:latest
+    docker push tarrow/wdqs-proxy:latest
 fi
--- wikibase/1.30/.travis/build-deploy.sh
+++ wikibase/1.30/.travis/build-deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #Oneline for full directory name see: https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker build "$DIR/../" -t wikibase/wikibase:1.30 -t wikibase/wikibase:latest
+docker build "$DIR/../" -t tarrow/wikibase:1.30 -t tarrow/wikibase:latest
 
 if [ "$SHOULD_DOCKER_PUSH" = true ]; then
-    docker push wikibase/wikibase:1.30
-    docker push wikibase/wikibase:latest
+    docker push tarrow/wikibase:1.30
+    docker push tarrow/wikibase:latest
 fi
